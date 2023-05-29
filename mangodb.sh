1840a87fa4e32afd0e47422050786c781ea19305
UmamaheshChedella <umamd92@gmail.com>
2023-05-30 05:06:33 +0530

    Roboshop-projects

--- mangodb.sh
+++ mangodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mInstalling MangoDB repo\e[0m"
-cp mangodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[33mInstalling MangoD B server\e[0m"
 yum install mongodb-org -y
 ##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf

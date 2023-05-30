7fc660ea2d0984463293ee49c1cc7a8eb4d69b0b
UmamaheshChedella <umamd92@gmail.com>
2023-05-30 07:24:02 +0530

    Roboshop-projects

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,14 @@
 cp frontend.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[33mInstalling NGNIX server\e[0m"
-yum install nginx -y
+yum install nginx -y | bash &>>/tmp/roboshop.log
 echo -e "\e[33mStart Ngnix server\e[0m"
 systemctl enable nginx
 systemctl start nginx
 echo -e "\e[33mRemove Files\e[0m"
 rm -rf /usr/share/nginx/html/*
 echo -e "\e[33mDownload Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip | bash &>>/tmp/roboshop.log
 echo -e "\e[33mUnzip Content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip | bash &>>/tmp/roboshop.log
 echo -e "\e[33m Configure service\e[0m"

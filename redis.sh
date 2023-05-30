b5a734fbe2606697bbf1b08cbcbadd48a37255e9
UmamaheshChedella <umamd92@gmail.com>
2023-05-31 04:03:39 +0530

    Roboshop-projects

--- redis.sh
+++ redis.sh
@@ -1,11 +1,11 @@
-echo -e "\e[33mDownloadinging rdis\e[0m"
+echo -e "\e[33mDownloading redis\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y | bash &>>/tmp/roboshop.log
-echo -e "\e[33mEnable redis\e[0m"
+echo -e "\e[33mEnable module redis\e[0m"
 yum module enable redis:remi-6.2 -y | bash &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling redis\e[0m"
 yum install redis -y | bash &>>/tmp/roboshop.log
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-echo -e "\e[33mStart redis\e[0m"
+echo -e "\e[33mStart redis service\e[0m"
 systemctl enable redis
 systemctl start redis

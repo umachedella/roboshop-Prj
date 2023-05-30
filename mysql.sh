39dd2a0933afaba3b397d10bfc160243bcad6e9d
UmamaheshChedella <umamd92@gmail.com>
2023-05-30 07:59:11 +0530

    Roboshop-projects

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+cp mysql.conf /etc/yum.repos.d/mysql.repo
+#disable MySQL 8 version
+yum module disable mysql -y
+echo -e "\e[33mDownloading MySQL\e[0m"
+yum install mysql-community-server -y
+echo -e "\e[33mStart MySQL\e[0m"
+systemctl enable mysqld
+systemctl start mysqld
+echo -e "\e[33mSet Password\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
+echo -e "\e[33mUser Created\e[0m"
+mysql -uroot -pRoboShop@1

13a41f330f37a76fa827bc0857a1ae61768f1e69
UmamaheshChedella <umamd92@gmail.com>
2023-05-30 07:38:16 +0530

    Roboshop-projects

--- user.sh
+++ user.sh
@@ -1,16 +1,25 @@
 cp user.conf /etc/systemd/system/user.service
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 echo -e "\e[33mInstallong NodeJS\e[0m"
-yum install nodejs -y | bash &>>/tmp/roboshop.log
+yum install nodejs -y | bash &>>/tmp/roboshop.log | bash &>>/tmp/roboshop.log
 echo -e "\e[33mUser created\e[0m"
 useradd roboshop
 echo -e "\e[33mCreated directory\e[0m"
 mkdir /app
 echo -e "\e[33m Unzip NodeJS\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip | bash &>>/tmp/roboshop.log
 cd /app
 unzip /tmp/user.zip
-echo -e "\e[33m Installing dependencies.\e[0m"
+echo -e "\e[33m Installing dependencies\e[0m"
 cd /app
-npm install
-echo -e "\e[33m Installing dependencies.\e[0m"
+npm install | bash &>>/tmp/roboshop.log
+echo -e "\e[33m Systemd reload\e[0m"
+systemctl daemon-reload
+echo -e "\e[33mStart user\e[0m"
+systemctl enable user
+systemctl start user
+echo -e "\e[33mInstallong MongoDB\e[0m"
+yum install mongodb-org-shell -y | bash &>>/tmp/roboshop.log
+echo -e "\e[33mShSchema Loaded\e[0m"
+mongo --host mongodb-dev.umamd.store </app/schema/user.js

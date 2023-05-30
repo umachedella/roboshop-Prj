54bf691f99996d0a6ade174a05ddd77d59ad53d4
UmamaheshChedella <umamd92@gmail.com>
2023-05-30 08:53:58 +0530

    Roboshop-projects

--- dispatch.sh
+++ dispatch.sh
@@ -1,20 +1,22 @@
 cp dispatch.conf /etc/systemd/system/dispatch.service
+echo -e "\e[33mInstalling golang\e[0m"
 yum install golang -y
-
+echo -e "\e[33mUser crated\e[0m"
 useradd roboshop
-
+echo -e "\e[33mDirectory crated\e[0m"
 mkdir /app
-
+echo -e "\e[33mDownloading content\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
+echo -e "\e[33m Unzip file\e[0m"
 unzip /tmp/dispatch.zip
-
+echo -e "\e[33m download the dependencies & build the software\e[0m"
 cd /app
 go mod init dispatch
 go get
 go build
-
+echo -e "\e[33m Start dispatch\e[0m"
 systemctl daemon-reload
-
+echo -e "\e[33m Start service\e[0m"
 systemctl enable dispatch
 systemctl start dispatch

980be5383b9aca6c2402d8e97330566c45b7a163
UmamaheshChedella <umamd92@gmail.com>
2023-05-30 05:54:53 +0530

    Roboshop-projects

--- mangodb.sh
+++ mangodb.sh
@@ -2,6 +2,7 @@ echo -e "\e[33mInstalling MangoDB repo\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[33mInstalling MangoD B server\e[0m"
 yum install mongodb-org -y
+sed-i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 ##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 echo -e "\e[33mStart MangoDB\e[0m"
 systemctl enable mongod

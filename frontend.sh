988a0c6ca41e9ab5511b3ccf631516fe9dfe858c
UmamaheshChedella <umamd92@gmail.com>
2023-05-31 19:21:42 +0530

    Roboshop-project

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,10 @@
-#cp /root/Roboshop-Prj/frontend.conf /etc/nginx/default.d/roboshop.conf
+cp frontend.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[33mInstalling NGNIX server\e[0m"
 yum install nginx -y | bash & >>/tmp/roboshop.log
 echo -e "\e[33mStart Ngnix server\e[0m"
 systemctl enable nginx
 systemctl start nginx
-cp frontend.conf /etc/nginx/default.d/roboshop.conf
+#cp frontend.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[33m Remove Files\e[0m"
 rm -rf /usr/share/nginx/html/*
 echo -e "\e[33mDownload Content\e[0m"

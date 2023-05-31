5bb1ea584da10ac64211b0c3356f54d9b2de1255
UmamaheshChedella <umamd92@gmail.com>
2023-05-31 19:26:12 +0530

    Roboshop-project

--- f.sh
+++ f.sh
@@ -7,6 +7,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp frontend.conf /etc/nginx/default.d/roboshop.conf
+cp /root/roboshop-Prj/frontend.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx

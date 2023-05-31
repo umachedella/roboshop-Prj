e864338447913515e97a538527af88a99e5c4444
UmamaheshChedella <umamd92@gmail.com>
2023-06-01 02:23:00 +0530

    Roboshop-project

--- payment.sh
+++ payment.sh
@@ -8,11 +8,12 @@ mkdir /app
 echo -e "\e[33mDownload the application code to created app directory\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip | bash &>>/tmp/roboshop.log
 cd /app
+echo -e "\e[33mExtracting Files to app directory\e[0m"
 unzip /tmp/payment.zip
-echo -e "\e[33m Download the dependencies\e[0m"
+echo -e "\e[33mDownload the dependencies\e[0m"
 pip3.6 install -r requirements.txt
 #pip3.6 install -r requirements.txt | bash &>>/tmp/roboshop.log
-echo -e "\e[33m Reload Sytemd\e[0m"
+echo -e "\e[33mReload Systemd\e[0m"
 systemctl daemon-reload
 echo -e "\e[33mStart Payment Service\e[0m"
 systemctl enable payment
